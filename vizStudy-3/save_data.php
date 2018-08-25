8ae64b38046cf4d46a1643065b68baebf7bff601
Preeti Ramaraj <preetir@umich.edu>
2018-08-25 14:33:40 -0700

    Modified save_data for each sub-study

---
+++ vizStudy-3/save_data.php
@@ -0,0 +1,15 @@
+<?php
+$blah=json_decode($_POST['blah'],true);
+
+if(!is_null($blah))
+{
+$id = rand();
+echo $id;
+$resultname = '../../../../../../../tmp/results3-' . $id . '.json';
+#$fp = fopen('../../../../../../tmp/results.json', 'w');
+$fp = fopen($resultname, 'w');
+fwrite($fp, json_encode($blah));
+fclose($fp);
+}
+
+?>
